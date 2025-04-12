591cc567f52835a409f7c78353e30069363e8446
Nikhil Dange <nikdange.me@gmail.com>
2025-04-12 20:38:18 +0530

    Issue-4 Refactor Poll Form UI

--- Polly/Poll Creation/PollCreationView.swift
+++ Polly/Poll Creation/PollCreationView.swift
@@ -16,50 +16,12 @@ struct PollCreationView: View {
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
-                VStack(alignment: .leading) {
-                    Text("Title")
-                    TextField("Enter poll title", text: $title)
-                        .modifier(TextFieldModifier())
-                    Text("Description")
-                    TextField("Enter poll description", text: $description)
-                        .modifier(TextFieldModifier())
-                    Text("Add Options:")
-                    HStack {
-                        TextField("Option Title", text: $addOption)
-                            .modifier(TextFieldModifier())
-                        Button {
-                            if addOption.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty { return }
-                            options.append(addOption)
-                        } label: {
-                            Image(systemName: "plus.circle")
-                        }
-                    }
+                HeaderView(title: $title,
+                           description: $description)
+                AddOptionView { title in
+                    options.append(title)
                 }
-                VStack(alignment: .leading) {
-                    Text("Existing Option:")
-                        .padding(.vertical)
-                        ForEach (options, id: \.self) { option in
-                            HStack {
-                                Text(option)
-                                Spacer()
-                                Button {
-                                    
-                                } label: {
-                                    Image(systemName: "pencil")
-                                }
-                                Button {
-                                    if let index = options.firstIndex(of: option) {
-                                        options.remove(at: index)
-                                    }
-                                } label: {
-                                    Image(systemName: "x.circle")
-                                }
-                            }
-                            .padding(8)
-                            .background(Color.gray.opacity(0.05).cornerRadius(8))
-                        }
-                }
-                
+                OptionListView(options: $options)
             }
             .padding()
         }
@@ -72,7 +34,7 @@ struct PollCreationView: View {
     }
 }
 
-struct TextFieldModifier: ViewModifier {
+private struct TextFieldModifier: ViewModifier {
     func body(content: Content) -> some View {
         content
             .padding(8)
@@ -81,6 +43,68 @@ struct TextFieldModifier: ViewModifier {
     }
 }
 
+private struct AddOptionView: View {
+    @State var titleText: String = ""
+    var addText: (String) -> Void
+    
+    var body: some View {
+        Text("Add Options:")
+        HStack {
+            TextField("Option Title", text: $titleText)
+                .modifier(TextFieldModifier())
+            Button {
+                if titleText.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty { return }
+                addText(titleText)
+            } label: {
+                Image(systemName: "plus.circle")
+            }
+        }
+    }
+}
+
+private struct OptionListView: View {
+    @Binding var options: [String]
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Existing Option:")
+                .padding(.vertical)
+            ForEach (options, id: \.self) { option in
+                HStack {
+                    Text(option)
+                    Spacer()
+                    Button {}
+                    label: {
+                        Image(systemName: "pencil")
+                    }
+                    Button {
+                        if let index = options.firstIndex(of: option) {
+                            options.remove(at: index)
+                        }
+                    } label: {
+                        Image(systemName: "x.circle")
+                    }
+                }
+                .padding(8)
+                .background(Color.gray.opacity(0.05).cornerRadius(8))
+            }
+        }
+    }
+}
+
+struct HeaderView: View {
+    @Binding var title: String
+    @Binding var description: String
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Title")
+            TextField("Enter poll title", text: $title)
+                .modifier(TextFieldModifier())
+            Text("Description")
+            TextField("Enter poll description", text: $description)
+                .modifier(TextFieldModifier())
+        }
+    }
+}
 
 #Preview {
     NavigationStack {
