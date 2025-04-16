69163fdeb6542f592e82a7961ae49cc4166d667b
Midhet Sulemani <midhetfatema94@gmail.com>
2025-04-16 19:37:35 +0530

    added pie charts

---
+++ Polly/Views/PollCharts/PollBarChartView.swift
@@ -0,0 +1,66 @@
+//
+//  PollBarChartView.swift
+//  Polly
+//
+//  Created by Midhet Sulemani on 12/04/25.
+//
+
+import SwiftUI
+import Charts
+
+struct PollBarChartView: View {
+    var data = SampleData.createSampleData()
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                ForEach(data.polls) { poll in
+                    VStack {
+                        Text(poll.title ?? String())
+                            .font(.title)
+                            .multilineTextAlignment(.center)
+                        
+                        chart(poll: poll)
+                        
+                        Divider()
+                            .padding()
+                    }
+                }
+            }
+        }
+        .padding()
+    }
+    
+    @ViewBuilder
+    private func chart(poll: Poll) -> some View {
+        let pollOptions: [PollOption] = poll.options?.toArray() ?? []
+        
+        Chart(pollOptions) { option in
+            let votes: [Vote] = option.votes?.toArray() ?? []
+            let optionTitle = option.text ?? String()
+            
+            Plot {
+                BarMark(
+                    x: .value("Participants", votes.count),
+                    y: .value("Choices", optionTitle),
+                    width: .fixed(100),
+                    height: .fixed(20)
+                )
+                .foregroundStyle(by: .value("Choices", optionTitle))
+            }
+            .accessibilityLabel("Poll Bar chart")
+            .symbol(by: .value("Choices", optionTitle))
+            .interpolationMethod(.catmullRom)
+            .position(by: .value("Choices", optionTitle))
+        }
+        .chartLegend(.visible)
+        .chartLegend(position: .top)
+        .chartYAxis(.hidden)
+        .chartXAxis(.visible)
+        .frame(height: 200)
+    }
+}
+
+#Preview {
+    PollBarChartView()
+}
--- Polly/Views/PollCharts/PollPieChartView.swift
+++ Polly/Views/PollCharts/PollPieChartView.swift
@@ -1,14 +1,14 @@
 //
-//  PollCharts.swift
+//  PollPieChartView.swift
 //  Polly
 //
-//  Created by Midhet Sulemani on 12/04/25.
+//  Created by Midhet Sulemani on 16/04/25.
 //
 
 import SwiftUI
 import Charts
 
-struct PollCharts: View {
+struct PollPieChartView: View {
     var data = SampleData.createSampleData()
     
     var body: some View {
@@ -30,7 +30,7 @@ struct PollCharts: View {
         }
         .padding()
     }
-    
+        
     @ViewBuilder
     private func chart(poll: Poll) -> some View {
         let pollOptions: [PollOption] = poll.options?.toArray() ?? []
@@ -39,28 +39,15 @@ struct PollCharts: View {
             let votes: [Vote] = option.votes?.toArray() ?? []
             let optionTitle = option.text ?? String()
             
-            Plot {
-                BarMark(
-                    x: .value("Participants", votes.count),
-                    y: .value("Choices", optionTitle),
-                    width: .fixed(100),
-                    height: .fixed(20)
-                )
-                .foregroundStyle(by: .value("Choices", optionTitle))
-            }
-            .accessibilityLabel("Poll Bar chart")
-            .symbol(by: .value("Choices", optionTitle))
-            .interpolationMethod(.catmullRom)
-            .position(by: .value("Choices", optionTitle))
+            SectorMark(angle: .value(optionTitle, votes.count))
+                .foregroundStyle(by: .value(optionTitle, optionTitle))
+                .accessibilityLabel("Poll Bar chart")
         }
         .chartLegend(.visible)
-        .chartLegend(position: .top)
-        .chartYAxis(.hidden)
-        .chartXAxis(.visible)
         .frame(height: 200)
     }
 }
 
 #Preview {
-    PollCharts()
+    PollPieChartView()
 }
