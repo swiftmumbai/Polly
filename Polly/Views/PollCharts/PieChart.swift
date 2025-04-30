ed363ad1e8db021f1826967e88b47788c5a79bba
Midhet Sulemani <midhetfatema94@gmail.com>
2025-04-30 13:28:12 +0530

    pie chart animation added

--- Polly/Views/PollCharts/PieChart.swift
+++ Polly/Views/PollCharts/PieChart.swift
@@ -11,6 +11,8 @@ import Charts
 struct PieChart: View {
     let poll: Poll
     
+    @State private var isAnimate: Bool = false
+    
     var body: some View {
         let pollOptions: [PollOption] = poll.options?.toArray() ?? []
         
@@ -18,13 +20,17 @@ struct PieChart: View {
             let votes: [Vote] = option.votes?.toArray() ?? []
             let optionTitle = option.text ?? String()
             
-            SectorMark(angle: .value(optionTitle, votes.count))
+            SectorMark(angle: .value(optionTitle, isAnimate ? votes.count : 0))
                 .foregroundStyle(by: .value(optionTitle, optionTitle))
                 .accessibilityLabel("Poll Pie chart")
         }
         .chartLegend(.visible)
         .frame(height: 200)
-        .animation(.easeInOut, value: pollOptions)
+        .onAppear {
+            withAnimation(.easeInOut(duration: 1.5).delay(2)) {
+                isAnimate = true
+            }
+        }
     }
         
     
